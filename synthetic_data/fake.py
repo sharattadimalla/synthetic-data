c7dc9bf4bd44997b11ac2c44c931349ced74f58a
Sharat C Tadimalla <sharat.tadimalla@gmail.com>
2024-11-05 19:45:26 +0530

    added ctgan

--- synthetic_data/fake.py
+++ synthetic_data/fake.py
@@ -3,6 +3,10 @@ from typing import Union
 from faker import Faker
 from pydantic import BaseModel, UUID4
 from random_address import real_random_address
+import pandas as pd
+from ctgan import CTGAN
+from sdv.single_table import CTGANSynthesizer
+from sdv.metadata import Metadata
 
 fake = Faker("es_ES")
 
@@ -23,11 +27,13 @@ class Person(BaseModel):
 
 class SynthethicData:
 
-    def __init__(self, num_records: int):
+    def __init__(self, num_records: int, method: str):
         self.num_records = num_records
+        self.method = method
         self.data = self._generate_synthetic_data()
 
     def _generate_single_record(self):
+
         us_address = real_random_address()
         address_line1 = us_address.get("address1")
         address_line2 = us_address.get("address2")
@@ -53,6 +59,17 @@ class SynthethicData:
         )
         return person.__dict__
 
+    def _generate_ctgan_sample(self):
+        df = pd.read_csv("output/customer_synthetic_data.csv")
+        metadata = Metadata.detect_from_dataframe(data=df, table_name="employee")
+        synthesizer = CTGANSynthesizer(metadata)
+        synthesizer.fit(df)
+        synthetic_data = synthesizer.sample(num_rows=self.num_records)
+        return synthetic_data
+
     def _generate_synthetic_data(self):
-        data = [self._generate_single_record() for x in range(self.num_records)]
+        if self.method == "faker":
+            data = [self._generate_single_record() for x in range(self.num_records)]
+        elif self.method == "ctgan":
+            data = self._generate_ctgan_sample()
         return data
